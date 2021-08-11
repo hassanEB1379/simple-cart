ac8b947a746323eaee33f76f10031f0d007613da
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-08-11 10:52:38 +0430

    update store.js file

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+// reducers
+import filterReducer from "../features/filter/filterSlice";
+import productsReducer from "../features/products/productSlice";
+import cartReducer from "../features/cart/cartSlice";
+import authReducer from "../features/auth/authSlice";
+
 export default configureStore({
-  reducer: {},
+    reducer: {
+        filter: filterReducer,
+        products: productsReducer,
+        cart: cartReducer,
+        auth: authReducer,
+    },
 });
