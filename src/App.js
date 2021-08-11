cdba2a207affceb56391e5b98bbad6456d37cfb7
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-08-11 10:44:27 +0430

    add layout routes to app.js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+// routes layouts components
+import MainLayout from "./app/layouts/MainLayout";
+import AuthLayout from "./app/layouts/AuthLayout";
 
 function App() {
-  return <div></div>;
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path={"/auth"} component={AuthLayout} />
+                <Route path={"/"} component={MainLayout} />
+            </Switch>
+        </BrowserRouter>
+    );
 }
 
 export default App;
