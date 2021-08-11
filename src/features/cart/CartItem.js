a1755fdef9464d22d89f86e0bf27760b5f54efb8
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-08-11 10:45:17 +0430

    create cart item component

---
+++ src/features/cart/CartItem.js
@@ -0,0 +1,60 @@
+import { Button, Card } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import {
+    countItemDecrement,
+    countItemIncrement,
+    removeOneFromCart,
+} from "./cartSlice";
+import Counter from "../../app/components/Counter";
+
+const CartItem = ({ data }) => {
+    const dispatch = useDispatch();
+
+    // handle counter
+    const increment = () => {
+        dispatch(countItemIncrement(data.id));
+    };
+    const decrement = () => {
+        dispatch(countItemDecrement(data.id));
+    };
+
+    const handleDeleteCartItem = () => {
+        dispatch(removeOneFromCart(data.id));
+    };
+
+    return (
+        <Card border="primary" className="gap-4 rounded mb-3">
+            <div className="p-4 d-flex gap-4">
+                <Card.Img
+                    style={{ width: 125, height: 125, objectFit: "contain" }}
+                    src={data.image}
+                />
+
+                <div>
+                    <Card.Title>{data.title}</Card.Title>
+
+                    <Card.Text>
+                        count :
+                        <Counter
+                            count={data.count}
+                            increment={increment}
+                            decrement={decrement}
+                        />
+                    </Card.Text>
+
+                    <Card.Text>
+                        total price : {Math.floor(data.count * data.price)} $
+                    </Card.Text>
+                </div>
+            </div>
+
+            <Card.Footer className="d-flex justify-content-end">
+                <Button onClick={handleDeleteCartItem} variant="danger">
+                    Delete
+                </Button>
+            </Card.Footer>
+        </Card>
+    );
+};
+
+export default CartItem;
