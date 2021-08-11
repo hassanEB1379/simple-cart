6bc5c7489db07473c1d7559908d7a636152ec351
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-08-11 10:41:20 +0430

    create private and public route components

---
+++ src/app/components/routes/PrivateRoute.js
@@ -0,0 +1,21 @@
+import { Redirect, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+const PrivateRoute = props => {
+    const { component: Component, ...rest } = props;
+
+    const userAuthenticated = useSelector(({ auth }) => auth.isAuthenticated);
+
+    const render = renderProps => {
+        if (userAuthenticated) {
+            return <Component {...renderProps} />;
+        } else {
+            // redirect to login page if user not logged in
+            return <Redirect to="/auth/login" />;
+        }
+    };
+
+    return <Route {...rest} render={render} />;
+};
+
+export default PrivateRoute;
---
+++ src/app/components/routes/PublicRoutes.js
@@ -0,0 +1,21 @@
+import { Redirect, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+const PublicRoutes = props => {
+    const { component: Component, restricted = false, ...rest } = props;
+
+    const userAuthenticated = useSelector(({ auth }) => auth.isAuthenticated);
+
+    const render = renderProps => {
+        if (userAuthenticated && restricted) {
+            // return user to panel if user authenticated
+            return <Redirect to="/" />;
+        } else {
+            return <Component {...renderProps} />;
+        }
+    };
+
+    return <Route {...rest} render={render} />;
+};
+
+export default PublicRoutes;
