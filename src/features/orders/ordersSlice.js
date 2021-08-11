a4b8b241c2066b2ca6d60a6c344abc19c0454219
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-08-11 21:44:45 +0430

    create orders slice

---
+++ src/features/orders/ordersSlice.js
@@ -0,0 +1,30 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { getStateLs } from "../../utils/localStorage";
+
+const ordersSlice = createSlice({
+    name: "orders",
+    initialState: getStateLs("orders") || [],
+    reducers: {
+        addOrders: {
+            reducer: (state, action) => {
+                state.push(action.payload);
+            },
+            prepare: orders => {
+                return {
+                    payload: {
+                        id: nanoid(),
+                        orderDate: new Date().toString(),
+                        data: orders,
+                    },
+                };
+            },
+        },
+        clearOrders: () => {
+            return [];
+        },
+    },
+});
+
+export const { addOrders, clearOrders } = ordersSlice.actions;
+
+export default ordersSlice.reducer;
