c5c73b28535a994e2865330b6743748fc4a65887
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-08-11 10:40:49 +0430

    create auth layout

---
+++ src/app/layouts/AuthLayout.js
@@ -0,0 +1,30 @@
+import { Container, Row, Col } from "react-bootstrap";
+import { Switch, Redirect } from "react-router-dom";
+import PublicRoutes from "../components/routes/PublicRoutes";
+
+import LoginForm from "../../features/auth/LoginForm";
+
+const AuthLayout = () => {
+    return (
+        <div className={"bg-light vh-100 d-flex align-items-center "}>
+            <Container fluid={"sm"}>
+                <Row className={"w-100"}>
+                    <Col lg={6} className={"mx-auto"}>
+                        <Switch>
+                            {/* routes */}
+                            <PublicRoutes
+                                restricted
+                                path={"/auth/login"}
+                                component={LoginForm}
+                            />
+
+                            <Redirect form={"/auth"} to={"/auth/login"} />
+                        </Switch>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
+};
+
+export default AuthLayout;
