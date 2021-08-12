c1389a6ee331f340cf770b8a6a6af0131a35e31b
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-08-12 13:00:54 +0430

    change cart page layout

--- src/features/cart/Cart.js
+++ src/features/cart/Cart.js
@@ -1,13 +1,18 @@
 import { useSelector } from "react-redux";
 import CartItem from "./CartItem";
 import { Col, Row } from "react-bootstrap";
+import CartSidebar from "./CartSidebar";
 
 const Cart = () => {
     const cartData = useSelector(state => state.cart);
 
     return (
         <Row>
-            <Col xs={8}>
+            <Col xs={12} lg={4}>
+                <CartSidebar />
+            </Col>
+
+            <Col xs={12} lg={8}>
                 {cartData.map(item => (
                     <CartItem data={item} key={item.id} />
                 ))}
