2aafbb1bba28e89c1174a7dc0d97fb98492c3b92
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-08-12 13:07:08 +0430

    update main layout component

--- src/app/layouts/MainLayout.js
+++ src/app/layouts/MainLayout.js
@@ -1,23 +1,37 @@
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
 import { Container } from "react-bootstrap";
-import { Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 
 import Cart from "../../features/cart/Cart";
 import ProductList from "../../features/products/ProductList";
+import Orders from "../../features/orders/Orders";
+import PrivateRoute from "../components/routes/PrivateRoute";
 
 const MainLayout = () => {
     return (
-        <>
+        <div
+            style={{ minHeight: "100vh" }}
+            className="d-flex flex-column justify-content-between"
+        >
             <Navbar />
 
-            <Container className="mt-5">
-                <Switch>
-                    {/* routes */}
-                    <Route path="/cart" component={Cart} />
-                    <Route path="/" component={ProductList} />
-                </Switch>
-            </Container>
-        </>
+            <main className="my-5 flex-grow-1">
+                <Container>
+                    <Switch>
+                        {/* routes */}
+                        <PrivateRoute path="/orders" component={Orders} />
+                        <Route path="/cart" component={Cart} />
+                        <Route path="/:category" component={ProductList} />
+
+                        <Redirect from="/" to="/all" />
+                    </Switch>
+                </Container>
+            </main>
+
+            <Footer />
+        </div>
     );
 };
 
