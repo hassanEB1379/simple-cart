803b6934c3185f5da6ca6544a561208ed5bfde0b
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-08-16 17:19:30 +0430

    add filter reducer to store

--- src/app/store.js
+++ src/app/store.js
@@ -5,6 +5,7 @@ import productsReducer from "../features/products/productSlice";
 import cartReducer from "../features/cart/cartSlice";
 import userReducer from "../features/user/userSlice";
 import ordersReducer from "../features/orders/ordersSlice";
+import filterReducer from "../features/filter/filterSlice";
 
 export default configureStore({
     reducer: {
@@ -12,5 +13,6 @@ export default configureStore({
         cart: cartReducer,
         user: userReducer,
         orders: ordersReducer,
+        filter: filterReducer,
     },
 });
